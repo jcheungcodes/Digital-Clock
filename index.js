f1c4ab610f5f614c79082237b45bd9208928e99b
Jacky Cheung <cheungjacky631@gmail.com>
2022-12-17 20:29:21 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const hrs = document.getElementById('hour');
+const mins = document.getElementById('minute');
+const secs = document.getElementById('second');
+const ToD = document.getElementById('session');
+
+function timeDisplay(){
+    let timeDate = new Date();
+    let hr = timeDate.getHours();
+    let min = timeDate.getMinutes();
+    let sec = timeDate.getSeconds();
+    let session = ToD;
+
+    if(hr >= 12){
+        ToD.innerHTML = "PM";
+    }else{
+        ToD.innerHTML = "AM"
+    }
+    
+    if(hr <= 12){
+        hr = hr
+    }else{
+        hr = hr - 12
+    }
+
+    hrs.innerHTML = hr;
+    mins.innerHTML = min;
+    secs.innerHTML = sec;
+}
+setInterval(timeDisplay, 10)
